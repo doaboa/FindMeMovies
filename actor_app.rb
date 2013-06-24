74116011d4e265dfbb449135b9c4b3313af88a8e
MakerSquare Student <student@lab-7.att.net>
2013-06-24 16:51:32 -0500

    required gems

--- actor_app.rb
+++ actor_app.rb
@@ -1,3 +1,5 @@
+require 'rubygems'
+require 'ruby-tmdb'
 
 @movie_genres = {"Action" => 28, "Adventure" => 12, "Animation" => 16, "Comedy" => 35, "Crime" => 80, "Disaster" => 105, "Documentary" => 99, "Drama" => 18, "Eastern" => 82, "Fantasy" => 14, "History" => 36, "Holiday" => 10595, "Horror" => 27, "Music" => 10402, "Musical" => 22, "Mystery" => 9648, "Road Movie" => 115, "Science Fiction" => 878, "Short" => 10749, "Sport" => 9805, "Suspense" => 10748, "Thriller" => 53, "Western" => 37}
 
@@ -22,3 +24,4 @@ else
 #http://api.themoviedb.org/2.1/methods/Genres.getList
 #https://www.themoviedb.org/talk/51c8742a760ee33f08101f52
 #http://api.themoviedb.org/2.1/methods/Movie.browse
+#http://rubydoc.info/gems/ruby-tmdb/0.2.1/frames
