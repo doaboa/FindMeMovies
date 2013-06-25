d896e792d9e03c4a321bbaadcfae11115a1298f2
MakerSquare Student <jdaudier@outlook.com>
2013-06-25 10:43:18 -0500

    organized

--- actor_app.rb
+++ actor_app.rb
@@ -1,3 +1,5 @@
+#-----GEMS and APIs
+
 require 'rubygems'
 require 'ruby-tmdb'
 
@@ -5,11 +7,12 @@ Tmdb.api_key = "6e837186b09388ff716901a70d300f61"
 Tmdb.default_language = "en"
 
 
-
+#------Arrays
 
 @movie_genres = {"Action" => 28, "Adventure" => 12, "Animation" => 16, "Comedy" => 35, "Crime" => 80, "Disaster" => 105, "Documentary" => 99, "Drama" => 18, "Eastern" => 82, "Fantasy" => 14, "History" => 36, "Holiday" => 10595, "Horror" => 27, "Music" => 10402, "Musical" => 22, "Mystery" => 9648, "Road Movie" => 115, "Science Fiction" => 878, "Short" => 10749, "Sport" => 9805, "Suspense" => 10748, "Thriller" => 53, "Western" => 37}
 
 
+#-----Program Starts
 
 puts "Hi!  What movie genre do you feeling like watching today?  Either type the genre, or type 'list' to get a list of all genres."
 
@@ -29,12 +32,12 @@ end
 
 @genre_id = @movie_genres[@genre]
 
-#PLAY WITH THIS!!
+#-------Pulling from API
 
 TmdbMovie.browse(:order_by => "rating", :order => "desc", :genres => @genre_id, :min_votes => 5, :page => 1, :per_page => 10, :language => "en", :expand_results => true)
   
 
-  
+
 def self.browse(options)
   options = {
     :expand_results => false
@@ -59,7 +62,7 @@ def self.browse(options)
   
 end
 
+#--------Resources
 
-#http://api.themoviedb.org/2.1/methods/Genres.getList
 #http://api.themoviedb.org/2.1/methods/Movie.browse
 #http://rubydoc.info/gems/ruby-tmdb/0.2.1/frames
